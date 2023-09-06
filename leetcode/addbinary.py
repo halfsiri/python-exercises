ff2264967b812623d28eb8c463781eaa6382189f
halfsiri <thankyoudom@gmail.com>
2023-09-06 14:39:38 -0400

    adding binary numbers

---
+++ leetcode/addbinary.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+
+        # Initialize the result
+        result = ''
+
+        # Initialize the carry
+        carry = 0
+
+        # Traverse the string
+        for i in range(max_len - 1, -1, -1):
+            r = carry
+            r += 1 if a[i] == '1' else 0
+            r += 1 if b[i] == '1' else 0
+            result = ('1' if r % 2 == 1 else '0') + result
+
+            # Compute the carry.
+            carry = 0 if r < 2 else 1
+
+        if carry != 0:
+            result = '1' + result
+
+        return result
+
+
+test = Solution()
+a = "11"
+b = "1"
+test2 = test.addBinary(a,b)
+print(test2)
